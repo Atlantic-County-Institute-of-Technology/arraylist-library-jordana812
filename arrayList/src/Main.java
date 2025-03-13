431125eda7723e9b72c741fe0ce5a2910f837fcc
jordan3 <arri8330@student.acitech.org>
2025-03-13 13:07:34 -0400

    poop

--- arrayList/src/Main.java
+++ arrayList/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        ArrayList<ListofThings>
+
+        private static int iterativeSearch(int[] intArray, int low, int high, int target){
+            int midpoint;
+            while (low <= high) {
+                midpoint = (high + low) / 2;
+
+                if (intArray[midpoint] < target) {
+                    low = midpoint + 1;
+                }
+                else if (intArray[midpoint] > target) {
+                    high = midpoint - 1;
+                }
+                else{
+                    return midpoint;
+                }
+            }
+
+        }
+
+        private static int binarySearch(int[] intArray, int  int target){
+
+        }
     }
 }
--- arrayList/src/lbratry.java
+++ arrayList/src/lbratry.java
@@ -3,3 +3,20 @@ import java.util.ArrayList;
 public class lbratry {
     Arraylist<String> lbratry = new Arraylist<String>();
 }
+
+        private static int iterativeSearch(int[] intArray, int low, int high, int target) {
+            int midpoint;
+            while (low<=high){
+                midpoint = (high + low) /2;
+            }
+            if(intArray[midpoint] < target){
+                low = midpoint + 1;
+            }
+            else if(intArray[midpoint] < target){
+                low = midpoint - 1;
+            }
+            else
+                return midpoint;
+
+        }
+            return -1;
